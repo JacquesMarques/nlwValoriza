d100a608bf60a840b770d2606857191600a75797
jacques <jmarques1@gmail.com>
2021-06-21 21:57:13 -0300

    First commit - initialization

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, {Request, request, Response, response} from "express";
+
+const app = express();
+
+app.get("/test", (request, response) => {
+    return response.send("Olá NLW");
+});
+
+app.post("/test", (request, response) => {
+    return response.send("Olá NLW método Post");
+});
+
+app.listen(3000, () => console.log("Server is running NLW"));
+
