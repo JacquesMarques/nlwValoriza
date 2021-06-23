636cb3317d807e72f9f15a9d6f2bf288533328eb
jacques <jmarques1@gmail.com>
2021-06-23 08:13:48 -0300

    Fix: Aula 2 - finished

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import express from "express";
+import { router } from "./routers/routers";
 
 const app = express();
 
+app.use(express.json());
+
 import "./database";
 
-app.listen(3000, () => console.log("Server is running"));
+app.use(router);
+
+app.listen(3000, () => console.log(`Server is running on port: 3000`));
 
