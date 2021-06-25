b3ea1c3af826ebf53c2cfe35c39c2d8cc421374c
jacques <jmarques1@gmail.com>
2021-06-24 21:17:35 -0300

    Aula 04 - Start

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,40 @@
+import {getCustomRepository} from "typeorm";
+import {UserRepository} from "../repositories/UserRepository";
+import { compare } from "bcryptjs";
+import { sign  } from "jsonwebtoken";
+
+interface IAuthenticateUserRequest {
+    email: string,
+    password: string
+}
+
+export class AuthenticateUserService {
+    async execute({ email, password}: IAuthenticateUserRequest ) {
+        const userRepository = getCustomRepository(UserRepository);
+
+        // Verifica se email existe
+        const user = await userRepository.findOne({
+            email
+        });
+
+        if (!user) {
+           throw new Error("Email/Password incorrect");
+        }
+
+        // Verificar se senha está correta
+        const passwordMatch = await compare(password, user.password);
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        // Gerar Token
+        const token = sign({
+            email: user.email
+        }, "763f1cfa46571799852042a23aeaeb84", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return token;
+    }
+}
